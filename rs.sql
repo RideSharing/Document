ef471cfb290adc50478b73310bb01d584e74617d
thanh.tran <thanh.tran@enclave.vn>
2015-03-12 09:53:50 +0700

    Change database structure

--- rs.sql
+++ rs.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 11, 2015 at 11:39 AM
+-- Generation Time: Mar 12, 2015 at 03:53 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -33,13 +33,6 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `content` text NOT NULL,
   `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `comment`
---
-
-INSERT INTO `comment` (`comment_id`, `user_comment_id`, `comment_about_user_id`, `content`, `create_at`) VALUES
-(1, 1, 1, 'thử nghiệm gõ tiếng việt', '2015-03-11 03:06:17');
 
 -- --------------------------------------------------------
 
@@ -76,17 +69,17 @@ CREATE TABLE IF NOT EXISTS `feedback` (
 CREATE TABLE IF NOT EXISTS `itinerary` (
 `itinerary_id` int(11) NOT NULL,
   `driver_id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL,
   `start_address` varchar(255) NOT NULL,
-  `pick_up_address` varchar(255) NOT NULL,
-  `drop_address` varchar(255) NOT NULL,
+  `pick_up_address` varchar(255) DEFAULT NULL,
+  `drop_address` varchar(255) DEFAULT NULL,
   `end_address` varchar(255) NOT NULL,
   `leave_date` varchar(255) NOT NULL,
-  `duration` int(11) NOT NULL,
+  `duration` int(3) NOT NULL,
   `cost` float NOT NULL,
-  `description` varchar(255) NOT NULL,
+  `description` text,
   `status` int(1) NOT NULL,
-  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -99,20 +92,8 @@ CREATE TABLE IF NOT EXISTS `rating` (
 `rating_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `rating_user_id` int(11) NOT NULL,
-  `rating` int(3) NOT NULL,
+  `rating` int(1) NOT NULL,
   `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `role`
---
-
-CREATE TABLE IF NOT EXISTS `role` (
-`Role_id` int(11) NOT NULL,
-  `Name` varchar(20) NOT NULL,
-  `Create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -123,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `role` (
 
 CREATE TABLE IF NOT EXISTS `staff` (
 `staff_id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL,
+  `role` int(1) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` text NOT NULL,
   `fullname` varchar(50) NOT NULL,
@@ -152,13 +133,6 @@ CREATE TABLE IF NOT EXISTS `user` (
   `status` int(1) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`user_id`, `email`, `password`, `api_key`, `fullname`, `phone`, `personalID`, `personalID_img`, `link_avatar`, `created_at`, `status`) VALUES
-(1, 'thanhbkdn92@gmail.com', '$2a$10$41d49cf60ea07f1971e9eubD1Cl5JUHsoiRC3WIUl76hnoVgoG/oC', '845deb10788bb88514fa7c2d105305ae', NULL, NULL, NULL, NULL, NULL, '2015-03-11 08:43:22', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -169,10 +143,10 @@ CREATE TABLE IF NOT EXISTS `vehicle` (
 `vehicle_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `type` varchar(255) NOT NULL,
-  `vehicle_number` varchar(20) NOT NULL,
+  `license_plate` varchar(20) NOT NULL,
   `reg_cetificate` varchar(255) NOT NULL,
   `vehicle_number_img` varchar(255) NOT NULL,
-  `vehiclae_img` varchar(255) NOT NULL,
+  `vehicle_img` varchar(255) NOT NULL,
   `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -184,7 +158,7 @@ CREATE TABLE IF NOT EXISTS `vehicle` (
 -- Indexes for table `comment`
 --
 ALTER TABLE `comment`
- ADD PRIMARY KEY (`comment_id`);
+ ADD PRIMARY KEY (`comment_id`), ADD KEY `user_comment_id` (`user_comment_id`), ADD KEY `comment_about_user_id` (`comment_about_user_id`);
 
 --
 -- Indexes for table `driver`
@@ -196,25 +170,19 @@ ALTER TABLE `driver`
 -- Indexes for table `feedback`
 --
 ALTER TABLE `feedback`
- ADD PRIMARY KEY (`feedback_id`);
+ ADD PRIMARY KEY (`feedback_id`), ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `itinerary`
 --
 ALTER TABLE `itinerary`
- ADD PRIMARY KEY (`itinerary_id`);
+ ADD PRIMARY KEY (`itinerary_id`), ADD KEY `customer_id` (`customer_id`), ADD KEY `driver_id` (`driver_id`);
 
 --
 -- Indexes for table `rating`
 --
 ALTER TABLE `rating`
- ADD PRIMARY KEY (`rating_id`);
-
---
--- Indexes for table `role`
---
-ALTER TABLE `role`
- ADD PRIMARY KEY (`Role_id`), ADD KEY `Name` (`Name`);
+ ADD PRIMARY KEY (`rating_id`), ADD KEY `user_id` (`user_id`), ADD KEY `rating_user_id` (`rating_user_id`);
 
 --
 -- Indexes for table `staff`
@@ -232,7 +200,7 @@ ALTER TABLE `user`
 -- Indexes for table `vehicle`
 --
 ALTER TABLE `vehicle`
- ADD PRIMARY KEY (`vehicle_id`);
+ ADD PRIMARY KEY (`vehicle_id`), ADD KEY `user_id` (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -259,11 +227,6 @@ MODIFY `itinerary_id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `rating`
 MODIFY `rating_id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `role`
---
-ALTER TABLE `role`
-MODIFY `Role_id` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
@@ -278,6 +241,49 @@ MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 ALTER TABLE `vehicle`
 MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_comment_id`) REFERENCES `user` (`user_id`),
+ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`comment_about_user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `driver`
+--
+ALTER TABLE `driver`
+ADD CONSTRAINT `driver_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `itinerary`
+--
+ALTER TABLE `itinerary`
+ADD CONSTRAINT `itinerary_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `user` (`user_id`),
+ADD CONSTRAINT `itinerary_ibfk_3` FOREIGN KEY (`driver_id`) REFERENCES `driver` (`user_id`);
+
+--
+-- Constraints for table `rating`
+--
+ALTER TABLE `rating`
+ADD CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+ADD CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`rating_user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `vehicle`
+--
+ALTER TABLE `vehicle`
+ADD CONSTRAINT `vehicle_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `driver` (`user_id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
